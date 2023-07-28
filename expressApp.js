c8bc3654187413c511d0d0fa55a606dcd554576b
Zan <stankovic.zan@gmail.com>
2023-07-28 16:35:30 +0200

    odstranu node_modules

--- expressApp.js
+++ expressApp.js
@@ -11,5 +11,16 @@ app.listen(3000);
 app.get('/', function (request, response){
     //unge header funkcije nerabimo vec dajat, express sam naredi
     //zgleda da tudi status code
-    response.send('<p>Prva Stran</p>');
-})
+    //response.send('<p>Prva Stran</p>');
+    // to je pa da damo nazaj html page.
+    //ce damo samo ./views... bo to absolute path,
+    //zato smo mogli dodat se un root:
+
+    response.sendFile('./views/index.html', {root: __dirname})
+});
+
+app.get('/about', function (request, response){
+    //unge header funkcije nerabimo vec dajat, express sam naredi
+    //zgleda da tudi status code
+    response.sendFile('./views/about.html', {root: __dirname})
+});
