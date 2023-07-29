d8715838df950a94a9eecd1f70b3d65c5c93eaff
Zan <stankovic.zan@gmail.com>
2023-07-29 02:06:40 +0200

    7 posnetek nodejs crash course. Ejs done

--- expressApp.js
+++ expressApp.js
@@ -1,26 +1,38 @@
 //na tak nacin se vedno zacne express
 //importamo 
 const express = require('express');
+
 // shranimo v spremenljivko, instanco zgleda
 const app = express();
+
+app.set('view engine', 'ejs');
 
 //poslusamo zahtevam na portu 3000
 //podobno kot v surovem nodejs
 app.listen(3000);
 
+//home
 app.get('/', function (request, response){
-    //unge header funkcije nerabimo vec dajat, express sam naredi
-    //zgleda da tudi status code
-    //response.send('<p>Prva Stran</p>');
-    // to je pa da damo nazaj html page.
-    //ce damo samo ./views... bo to absolute path,
-    //zato smo mogli dodat se un root:
-
-    response.sendFile('./views/index.html', {root: __dirname})
+    const blogs = [
+        {title: 'yoshi je najde jajce', snippet: 'lorem lorem'},
+        {title: 'mario je najde jajce', snippet: 'lorem lorem'},
+        {title: 'kako premagat brskalnik', snippet: 'lorem lorem'}
+    ]
+    response.render('index', {title:'prva stran', blogs: blogs})
 });
 
+//about 
 app.get('/about', function (request, response){
-    //unge header funkcije nerabimo vec dajat, express sam naredi
-    //zgleda da tudi status code
-    response.sendFile('./views/about.html', {root: __dirname})
+    response.render('about', {naslov: 'About'})
 });
+
+
+
+app.get('/blogs/create', function (request, response){
+    response.render('create', {naslov: 'create a new blog'});
+})
+
+//404 page. Ce nismo prej nic poslali naazaj, se bo to zgodilo
+app.use(function (request, response){
+    response.status(404).render('404', {naslov: '404'});
+})
